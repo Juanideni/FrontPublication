e7d973ee8a9f93cad82a5096f84ef0ddea59c288
juanideni <juanidenipoti11@gmail.com>
2024-03-01 01:33:04 -0300

    random changes

--- src/main.js
+++ src/main.js
@@ -7,9 +7,5 @@ import router from './router'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-const API_URL_PORT = "https://localhost:44388/"
 
 
-export default {
-    API_URL_PORT
-}
--- src/services/PublicationService.js
+++ src/services/PublicationService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-
 const API_URL = "https://localhost:44388/api";
+
 
 const clientAPI = axios.create({
     baseURL: API_URL
